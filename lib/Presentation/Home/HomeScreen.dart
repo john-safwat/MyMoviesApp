fd8ebc259c98c8cfd97af7bc6c2d7f9fb85fddd6
john safwat <john362sss@gmail.com>
2023-02-23 18:45:40 +0200

    first update

---
+++ lib/Presentation/Home/HomeScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = 'Home Screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('john'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
