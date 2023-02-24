014660edcbfb1cdeff3ed5f3c66a106a5d037016
john safwat <john362sss@gmail.com>
2023-02-24 19:35:48 +0200

    add tabs and edit home screen

--- lib/Presentation/Home/HomeScreenView.dart
+++ lib/Presentation/Home/HomeScreenView.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mymoviesapp/Presentation/Home/HomeScreenViewModel.dart';
+import 'package:mymoviesapp/Presentation/Home/Tabs/Browse/BrowseTabView.dart';
+import 'package:mymoviesapp/Presentation/Home/Tabs/Home/HomeTabView.dart';
+import 'package:mymoviesapp/Presentation/Home/Tabs/Profile/ProfileTabVIew.dart';
+import 'package:mymoviesapp/Presentation/Home/Tabs/Search/SearchTabView.dart';
 import 'package:mymoviesapp/Presentation/Theme/Theme.dart';
 import 'package:provider/provider.dart';
 
@@ -11,6 +15,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int selectedIndex = 0;
+  List<Widget> tabs = [HomeTabView() , SearchTabView(), BrowseTabView() , ProfileTabView() ];
   HomeScreenViewModel viewModel = HomeScreenViewModel();
 
   @override
@@ -24,6 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return ChangeNotifierProvider(
       create: (_) => viewModel,
       child: Scaffold(
+        body: tabs[selectedIndex],
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: selectedIndex,
           onTap: (value) {
---
+++ lib/Presentation/Home/Tabs/Profile/ProfileTabVIew.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:mymoviesapp/Presentation/Theme/Theme.dart';
+
+class ProfileTabView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: MyTheme.gold,
+    );
+  }
+}
